bddfc0873a10c1954b5af0f5223ef17a52442af7
Zero Systems <isszero06@gmail.com>
2024-01-16 00:21:22 +0200

    Update __init__.py

--- picking_stock_available/models/__init__.py
+++ picking_stock_available/models/__init__.py
@@ -3,10 +3,11 @@
 # Author      : Zero For Information Systems (<www.erpzero.com>)
 # Copyright(c): 2016-Zero For Information Systems
 # All Rights Reserved.
-#
+#zerosystems #erp #odoo
 # This program is copyright property of the author mentioned above.
 # You can`t redistribute it and/or modify it.
 #
 #################################################################################
+
 
 from . import stock
